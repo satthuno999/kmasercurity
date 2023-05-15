b5b7e2296f3436bcb1762c6021eb7cd7b7e1ab6b
satthuno999 <binh9aqktk@gmail.com>
2023-05-15 22:18:08 +0700

    add CSP

--- js/app.js
+++ js/app.js
@@ -58,48 +58,48 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.head.appendChild(metaTag);
 
-//   WebFont.load({
-//     google: { families: ["Lato:300,400,700,900"] },
-//     custom: {
-//       families: [
-//         "Flaticon",
-//         "Font Awesome 5 Solid",
-//         "Font Awesome 5 Regular",
-//         "Font Awesome 5 Brands",
-//         "simple-line-icons",
-//       ],
-//       urls: [
-//         "http://192.168.1.5/nextcloud/apps/kmasercurity/css/fonts.min.css",
-//       ],
-//     },
-//     active: function () {
-//       sessionStorage.fonts = true;
-//     },
-//   });
+  //   WebFont.load({
+  //     google: { families: ["Lato:300,400,700,900"] },
+  //     custom: {
+  //       families: [
+  //         "Flaticon",
+  //         "Font Awesome 5 Solid",
+  //         "Font Awesome 5 Regular",
+  //         "Font Awesome 5 Brands",
+  //         "simple-line-icons",
+  //       ],
+  //       urls: [
+  //         "http://192.168.1.5/nextcloud/apps/kmasercurity/css/fonts.min.css",
+  //       ],
+  //     },
+  //     active: function () {
+  //       sessionStorage.fonts = true;
+  //     },
+  //   });
 
-  $.ajax({
-    type: "GET",
-    url: "http://14.225.254.142:8080/api/v1/models",
-    data: {},
-    success: function (jsondata) {
-      //document.getElementById('loading').style.display = 'none';
-      var subTitle = "";
-      console.log(jsondata);
-      alert(1);
-      if (jsondata.status === "success") {
-      } else {
-      }
-      document.title = subTitle + document.title;
-    },
-  });
+  //   $.ajax({
+  //     type: "GET",
+  //     url: "http://14.225.254.142:8080/api/v1/models",
+  //     data: {},
+  //     success: function (jsondata) {
+  //       //document.getElementById('loading').style.display = 'none';
+  //       var subTitle = "";
+  //       console.log(jsondata);
+  //       alert(1);
+  //       if (jsondata.status === "success") {
+  //       } else {
+  //       }
+  //       document.title = subTitle + document.title;
+  //     },
+  //   });
 
-  //   fetch(`http://14.225.254.142:8080/api/v1/models`, {
-  //     method: "GET",
-  //   })
-  //     .then((response) => response.json())
-  //     .then((response) => {
-  //       console.log(response);
-  //     })
-  //     .catch((error) => console.log(error));
+  fetch(`http://14.225.254.142:8080/api/v1/models`, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => console.log(error));
   //   OCA.kmasercurity.Core.init();
 });
