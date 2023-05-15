1b9f7867c0ab8103a880c32a8a44fe37938494ff
satthuno999 <binh9aqktk@gmail.com>
2023-05-15 23:42:22 +0700

    add CSP

--- js/app.js
+++ js/app.js
@@ -58,41 +58,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.head.appendChild(metaTag);
 
-  //   WebFont.load({
-  //     google: { families: ["Lato:300,400,700,900"] },
-  //     custom: {
-  //       families: [
-  //         "Flaticon",
-  //         "Font Awesome 5 Solid",
-  //         "Font Awesome 5 Regular",
-  //         "Font Awesome 5 Brands",
-  //         "simple-line-icons",
-  //       ],
-  //       urls: [
-  //         "http://192.168.1.5/nextcloud/apps/kmasercurity/css/fonts.min.css",
-  //       ],
-  //     },
-  //     active: function () {
-  //       sessionStorage.fonts = true;
-  //     },
-  //   });
-
-  //   $.ajax({
-  //     type: "GET",
-  //     url: "http://14.225.254.142:8080/api/v1/models",
-  //     data: {},
-  //     success: function (jsondata) {
-  //       //document.getElementById('loading').style.display = 'none';
-  //       var subTitle = "";
-  //       console.log(jsondata);
-  //       alert(1);
-  //       if (jsondata.status === "success") {
-  //       } else {
-  //       }
-  //       document.title = subTitle + document.title;
-  //     },
-  //   });
-
   fetch(`https://www.googleapis.com/books/v1/volumes?q=javascript`, {
     method: "GET",
   })
